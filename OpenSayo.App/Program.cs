446a4b4e36d9f61e17498e19505d90fb970c0008
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2025-04-18 20:50:53 -0500

    Add gitignore, csproject, taskfile and code

---
+++ OpenSayo.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenSayo.Device;
+
+namespace OpenSayo.App;
+
+class Program {
+	public static void Main()
+	{
+		Console.WriteLine("yo");
+		Color myColor = new Color(20, 20 ,20);
+		Console.WriteLine(myColor);
+	}
+}
---
+++ OpenSayo.Device/Utils.cs
@@ -0,0 +1,18 @@
+namespace OpenSayo.Device {
+	class Color {
+		public byte g;
+		public byte r;
+		public byte b;
+		
+		public Color(byte red, byte blue, byte green) {
+			r = red;
+			g = green;
+			b = blue;
+		}
+
+		override public string ToString()
+		{
+			return "(" + r + ", " + g + ", " + b + ")";
+		}
+	}
+}
