38e7e769e226ac91f0aa54314b04dfc353ef7b8d
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2025-04-19 15:28:05 -0500

    Add checksum algorithm

--- OpenSayo.Device/Utils.cs
+++ OpenSayo.Device/Utils.cs
@@ -1,8 +1,26 @@
 namespace OpenSayo.Device
 {
+	class Util {
+		// The algorithm that satisfies whatever checks bytes 0x02 and 0x03
+		public static void Checksum(byte[] buffer)
+		{
+			ushort num = 0;
+			for(int i = 0; i < buffer.Length; i += 2) {
+				if(i == 2) { continue; } // Just in case something happens to be here
+				
+				num += BitConverter.ToUInt16(buffer, i);
+			}
+
+			buffer[2] = (byte) (num & 0xFF);
+			buffer[3] = (byte) (num >> 8);
+		}
+	}
+
+
+
 	class Color {
-		public byte g;
 		public byte r;
+		public byte g;
 		public byte b;
 		
 		public Color(byte red, byte blue, byte green) {
